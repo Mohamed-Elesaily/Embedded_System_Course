e7760c4fdc8487b1194e4cff6a0496992e0698df
msm <msm@msm-Lenovo-G500>
2019-07-31 16:10:54 +0200

    Finishing coding

--- stats.h
+++ stats.h
@@ -9,13 +9,14 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.h
+ * 
+ * @brief  In this module have functions definition
  *
- * <Add Extended Description Here>
+ *  <Add Extended Description Here>
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Mohamed Elesaily
+ * @date 30/7/2019
  *
  */
 #ifndef __STATS_H__
@@ -24,17 +25,88 @@
 /* Add Your Declarations and Function Comments here */ 
 
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief  Print function 
  *
- * <Add Extended Description Here>
+ * It take one paramter and print it
+ * 
+ * @param parm it can be median, mean or any thing but must be integer   
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- *
- * @return <Add Return Informaiton here>
+ * @return doesn't return any thing
+ */
+void print_statistics(int parm);
+
+
+
+/**
+ * @brief Function that take array, his size and print array elements
+ * 
+ * @param ptr It pointer that points to first element address  
+ * @param size Integer value contains array size
+ * 
+ * @return doesn't return any thing
+ */
+void print_array(unsigned char * ptr, int size);
+
+/**
+ * @brief Function return median
+ * 
+ * @param ptr It pointer that points to first element address  
+ * @param size Integer value contains array size
+ * 
+ * @return median
+ */
+int find_median(unsigned char *ptr, int index);
+
+
+
+
+
+/**
+ * @brief Function return mean
+ * 
+ * @param ptr It pointer that points to first element address  
+ * @param size Integer value contains array size
+ * 
+ * @return mean
+ */
+int find_mean(unsigned char *ptr, int size);
+
+
+
+/**
+ * @brief Function return maximum
+ * 
+ * @param ptr It pointer that points to first element address  
+ * @param size Integer value contains array size
+ * 
+ * array must be sorted by sort_array function
+ * 
+ * @return maximum
+ */
+int find_maximum(unsigned char *ptr, int size);
+
+
+
+/**
+ * @brief Function return minimum
+ * 
+ * @param ptr It pointer that points to first element address  
+ * @param size Integer value contains array size
+ * 
+ * array must be sorted by sort_array function
+ * 
+ * @return minimum
+ */
+int find_minimum(unsigned char *ptr, int size);
+/**
+ * @brief Function that sort array
+ * 
+ * @param ptr It pointer that points to first element address  
+ * @param size Integer value contains array size
+ * 
+ * @return doesn't return any thing
  */
 
 
+void sort_array(unsigned char *ptr, int size);
 #endif /* __STATS_H__ */
